6b3647feec79ceab07c13cc515fb48bf5b240f63
farhadhossain6996 <farhadhossain6996@gmail.com>
2026-09-25 02:51:14 +0600

    almost done

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -5,55 +5,66 @@ import React from 'react'
 
 const SinglePage = () => {
   return (
-    <div className='px-4 md:px-8 xl:32 2xl:px-64 resize flex flex-col  lg:flex-row gap-16 '>
-      {/* IMG */}
-<div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-<ProductImages/>
-</div>
-{/* Texts */}
-<div className="w-full lg:w-1/2 flex flex-col gap-6 ">
+    <div className='px-4 md:px-8 xl:px-32 2xl:px-64 flex flex-col lg:flex-row gap-16 py-8'>
+      
+      {/* Product Images Section (Sticky on Large Screens) */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
 
-<div className="mt-12">
-<h1 className=' text-4xl font-medium' >Product Name</h1>
-<p className='text-gray-500'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore totam vel earum veniam incidunt, veritatis ex error impedit perspiciatis beatae.</p>
-<div className='h-2 bg-gray-100 '/>
-<div className="flex items-center gap-4">
+      {/* Product Details Section */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        
+        {/* Main Info */}
+        <div className="flex flex-col gap-4">
+          <h1 className='text-3xl lg:text-4xl font-medium'>Product Name</h1>
+          <p className='text-gray-500 text-sm lg:text-base'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore totam vel earum veniam incidunt, veritatis ex error impedit perspiciatis beatae.
+          </p>
+        </div>
 
-  <h3 className='text-xl text-gray-500 line-through'>$59</h3>
-  <h2 className=' font-medium text-2xl'>$49</h2>
-</div>
-<div className='h-2 bg-gray-100 '/>
-<CustomizeProducts/>
-<Add/>
+        <div className='h-[1px] bg-gray-200 w-full' />
 
-</div>
+        {/* Pricing */}
+        <div className="flex items-center gap-4">
+          <h3 className='text-xl text-gray-400 line-through'>$59</h3>
+          <h2 className='font-semibold text-2xl lg:text-3xl text-gray-800'>$49</h2>
+        </div>
 
-<div className="h-2 bg-gray-100">
-<div className="text-sm">
-  <h4 className='font-medium'>Title</h4>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in adipisci sapiente sunt, id facere totam error iste enim earum eveniet tempora autem eaque, eos quasi excepturi, est consequuntur cumque.</p>
-</div>
-<div className="text-sm">
-  <h4 className='font-medium'>Title</h4>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in adipisci sapiente sunt, id facere totam error iste enim earum eveniet tempora autem eaque, eos quasi excepturi, est consequuntur cumque.</p>
-</div>
-<div className="text-sm">
-  <h4 className='font-medium'>Title</h4>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in adipisci sapiente sunt, id facere totam error iste enim earum eveniet tempora autem eaque, eos quasi excepturi, est consequuntur cumque.</p>
-</div>
+        <div className='h-[1px] bg-gray-200 w-full' />
 
+        {/* Customization & Add to Cart */}
+        <div className="flex flex-col gap-6">
+          <CustomizeProducts />
+          <Add />
+        </div>
 
+        <div className='h-[1px] bg-gray-200 w-full' />
 
-</div>
+        {/* Additional Info / Accordion-style descriptions */}
+        <div className="flex flex-col gap-4 text-gray-600">
+          <div className="text-sm flex flex-col gap-1">
+            <h4 className='font-medium text-gray-800'>Title 1</h4>
+            <p className='text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in adipisci sapiente sunt.</p>
+          </div>
+          
+          <div className="text-sm flex flex-col gap-1">
+            <h4 className='font-medium text-gray-800'>Title 2</h4>
+            <p className='text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in adipisci sapiente sunt.</p>
+          </div>
+          
+          <div className="text-sm flex flex-col gap-1">
+            <h4 className='font-medium text-gray-800'>Title 3</h4>
+            <p className='text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in adipisci sapiente sunt.</p>
+          </div>
+          
+          <div className="text-sm flex flex-col gap-1">
+            <h4 className='font-medium text-gray-800'>Title 4</h4>
+            <p className='text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique in adipisci sapiente sunt.</p>
+          </div>
+        </div>
 
-
-
-
-</div>
-
-
-
-
+      </div>
     </div>
   )
 }
--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -37,9 +37,9 @@ onClick={handleProfile}
       <Bell className="w-6 h-6" />
 
 
-<div className="relative cursor-pointer">
+<div className="relative cursor-pointer" onClick={()=> setIsCartOpen((prev)=> !prev)}>
         <ShoppingCart className="w-6 h-6"
-      onClick={()=> setIsCartOpen((prev)=> !prev)} />
+       />
 
       <div className="absolute -top-4 -right-4 w-6 h-6 bg-primary rounded-full text-white flex items-center justify-center  ">2</div>
 {
